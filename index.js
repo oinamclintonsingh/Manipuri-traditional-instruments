cf4df4d47cbf3f5495b2fb8da395a4066c1c5cd6
Oinam Clinton Singh <122223211+oinamclintonsingh@users.noreply.github.com>
2024-01-07 00:51:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,37 +15,37 @@ document.addEventListener("keydown", function(event) {
 
 function makeSound(key) {
     switch (key) {
-        case "l":
+        case "a":
             var tom1 = new Audio("sounds/Langden_01.mp3");
             tom1.play();
             break;
 
-        case "p":
+        case "b":
             var tom2 = new Audio("sounds/Pena_01.mp3");
             tom2.play();
             break;
         
-        case "m":
+        case "c":
             var tom3 = new Audio("sounds/Moibung_01.mp3");
             tom3.play();
             break;
 
-        case "k":
+        case "d":
             var tom4 = new Audio("sounds/Kortan_01.mp3");
             tom4.play();
             break;
 
-        case "s":
+        case "e":
             var crash = new Audio("sounds/Sambung_01.mp3");
             crash.play();
             break;
 
-        case "b":
+        case "f":
             var kick = new Audio("sounds/Pung_01.mp3");
             kick.play();
             break;
 
-        case "j":
+        case "g":
             var snare = new Audio("sounds/Jian_01.mp3");
             snare.play();
             break;
@@ -55,15 +55,3 @@ function makeSound(key) {
             break;
     }
 }
-
-
-
-//cahnging the color of the letter at the image.
-// document.querySelectorAll(".drum")[i].addEventListener("click", handleClick );
-    // function handleClick() {
-    //     this.style.color = "white";
-    // }
-
-
-
-
